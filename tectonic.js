bddec396f9d25aa591d773bc9b6e018be648c530
Corin Lawson <au-phiware@users.noreply.github.com>
2016-03-04 16:01:21 +1100

    UMD wrapper
    As per https://github.com/umdjs/umd/blob/d31bb6ee7098715e019f52bdfe27b3e4bfd2b97e/templates/returnExports.js

--- tectonic.js
+++ tectonic.js
@@ -1,3 +1,18 @@
+(function (root, factory) {
+    if (typeof define === 'function' && define.amd) {
+        // AMD. Register as an anonymous module.
+        define([], factory);
+    } else if (typeof module === 'object' && module.exports) {
+        // Node. Does not work with strict CommonJS, but
+        // only CommonJS-like environments that support module.exports,
+        // like Node.
+        module.exports = factory();
+    } else {
+        // Browser globals (root is window)
+        root.Tectonic = factory();
+  }
+}(this, function () {
+
 function Tectonic(element, basis) {
   if (!(element instanceof Node)) {
     element = element.get(0);
@@ -759,3 +774,6 @@ Tectonic.toggleClass = function(className, attr, inverse) {
     read(data, selected);
   });
 };
+
+return Tectonic;
+}));
